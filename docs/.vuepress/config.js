c19bf66272bee577135450da2db288fcb7e31c16
wenreq <294491328@qq.com>
2022-05-30 23:31:43 +0800

    'feat(ts):枚举类型'

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -193,7 +193,8 @@ module.exports = {
                   '/technology/typescript/lagou/06.md',
                   '/technology/typescript/lagou/07.md',
                   '/technology/typescript/lagou/08.md',
-                  '/technology/typescript/lagou/09.md'
+                  '/technology/typescript/lagou/09.md',
+                  '/technology/typescript/lagou/10.md'
                 ]
               }
             ]
